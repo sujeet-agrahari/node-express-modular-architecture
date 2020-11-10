967f1b5c2272713a6b0c985a20e2272f22173caa
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2020-11-10 09:00:31 +0530

    Remove catching sepecific db errors

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -23,16 +23,6 @@ module.exports = async (err, req, res, next) => {
   if (err instanceof APIError) {
     return res.status(err.status).send({ success: false, message: err.message });
   }
-  // catch all sequelize errors
-  if (err instanceof db.ConnectionRefusedError) {
-    return res.status(500).send({ success: false, message: 'Could not connect to database!' });
-  } if (err instanceof db.ConnectionError) {
-    return res.status(408).send({ success: false, message: 'Connection timed out!' });
-  } if (err instanceof db.ForeignKeyConstraintError) {
-    return res.status(409).send({ success: false, message: 'Failed to insert data!' });
-  } if (err instanceof db.UniqueConstraintError) {
-    return res.status(409).send({ success: false, message: err.errors[0].message });
-  }
   // connect all errors
   return res.status(500).send({ success: false, message: 'Something went wrong!' });
 };
