58d8d33bb268a129b3bbad59a569b71746f734e5
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2021-01-28 09:05:29 +0530

    Separate models, seeders and migrations to support them

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,8 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const glob = require('glob');
 
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require(`${__dirname}/../../config/config.json`)[env];
 const db = {};
 
 let sequelize;
@@ -12,16 +14,20 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-glob.sync(`${__dirname} + /../**/components/*.model.js`)
+fs
+  .readdirSync(__dirname)
+  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
-    const model = sequelize.import(file);
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
+
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
