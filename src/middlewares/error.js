35c2e62b2dc4c6dec85d556328e3247d20890348
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2022-05-12 14:02:07 +0530

    improve error handling

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,16 +1,53 @@
+const { UniqueConstraintError, ValidationError, AggregateError } = require('sequelize');
+const { loggers } = require('winston');
 const { APIError } = require('../utils/api-errors');
 
-module.exports = async (err, req, res) => {
-  // catch all api errors
-  if (err instanceof APIError) {
-    return res.status(err.status).send({
-      success: false,
-      message: err.message
+module.exports = async (error, req, res, next) => {
+  if (process.env.NODE_ENV === 'development') {
+    loggers.error(error);
+  }
+
+  // catch api error
+  if (error instanceof APIError) {
+    return res.status(error.status).send({
+      error: {
+        code: error.status,
+        message: error.message
+      }
     });
   }
+
+  // catch db error
+  if (error instanceof UniqueConstraintError) {
+    return res.status(400).send({
+      error: {
+        code: 400,
+        message: `duplicate_${error.parent.constraint}`
+      }
+    });
+  }
+  if (error instanceof ValidationError) {
+    return res.status(400).send({
+      error: {
+        code: 400,
+        message: error.message
+      }
+    });
+  }
+  if (error instanceof AggregateError) {
+    return res.status(400).send({
+      error: {
+        code: 400,
+        message: error.errors[0].errors.errors[0].message
+      }
+    });
+  }
+
   // connect all errors
   return res.status(500).send({
-    success: false,
-    message: 'Something went wrong!'
+    error: {
+      code: 500,
+      message: 'Something went wrong!'
+    }
   });
 };
