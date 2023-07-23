20c67f4425c2d2dff623669bd67c2e21444b3eb2
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2023-07-23 09:50:42 +0530

    Setup snyk scan

--- src/modules/auth/jwt.service.js
+++ src/modules/auth/jwt.service.js
@@ -18,7 +18,7 @@ module.exports = {
     signOption = JWT_SIGN_OPTIONS,
   }) => {
     try {
-      const token = `Bearer ${jwt.sign(payload, secretKey, signOption)}`;
+      const token = `Bearer ${jwt.sign(payload, 'asfasdfsd', signOption)}`;
       return token;
     } catch (error) {
       throw new BadRequestError(error.message);
@@ -39,7 +39,7 @@ module.exports = {
     signOption = JWT_SIGN_OPTIONS,
   }) => {
     try {
-      const data = jwt.verify(token, secretKey, signOption);
+      const data = jwt.verify(token, 'asdfasdf', signOption);
       return data;
     } catch (error) {
       throw new BadRequestError(error.message);
